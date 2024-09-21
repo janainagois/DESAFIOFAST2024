c1d34086bb00fb87aa11174e33db44d43dded03c
IanPerigoVianna <ian.p.v@outlook.com>
2024-09-21 12:32:10 -0300

    Corrigido blocos

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,33 +1,21 @@
-# Recurso google_compute_instance
+resource "google_compute_instance" "app_instance" {
+  provider = google
+  name     = "swarm-node-${count.index}"
+  machine_type = var.machine_type
+  zone     = var.zone
 
-resource "google_compute_instance" "app_instance"{
-    #resource_service_account
-    provider = google # vai acessar meu provider.tf
-    display_name = "Service Account for Swarm"
-
-    name = "swarm-node-${count.index}"
-    machine_type = var.machine_type
-    zone = var.zone
-    min_cpu_platform = ""
-    #opcional 
-    metadata= {
-        ssh-keys = "ansible: ${file(var.ssh_key_path_default_user)}"
+  boot_disk {
+    initialize_params {
+      image = var.google_instance_image
     }
-}
+  }
 
-boot_disk{
-    initialize_params{
-        image = var.google_instance_image
-    }
-}
-
-network_interface{
+  network_interface {
     network = var.google_network
-    access_config{}
-}
+    access_config {}
+  }
 
-metadata_startup_script = {
-    // You can pass Docker installation commands or scripts here
+  metadata = {
     "startup-script" = <<-EOT
       #!/bin/bash
       apt-get update
@@ -35,22 +23,23 @@ metadata_startup_script = {
       systemctl start docker
       systemctl enable docker
       docker swarm init || true
-      # Optionally join other nodes
     EOT
   }
 
-tags = ["swarm-node"]
+  tags = ["swarm-node"]
 
-
-#documentação
-service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    #email  = google_service_account.default.email opcional
+  service_account {
+    // Google recommends custom service accounts with cloud-platform scope
+    email  = google_service_account.default.email // opcional
     scopes = ["cloud-platform"]
   }
+}
 
-#Opicional
-#retorna o IP da instância
+resource "google_service_account" "default" {
+  account_id   = "swarm-service-account"
+  display_name = "Service Account for Swarm"
+}
+
 output "instance_ips" {
-  value = google_compute_instance.swarm_node.*.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.app_instance.*.network_interface[0].access_config[0].nat_ip
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,16 @@
 terraform {
+    
     required_version = "1.9.5"
-}
 
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "6.3.0"
+
+    required_providers {
+        google = {
+        source = "hashicorp/google"
+        version = "6.3.0"
+        }
     }
-  }
 
-backend "local" {
-    path = "terraform.tfstate"
+    backend "local" {
+        path = "terraform.tfstate"
+    }
 }
