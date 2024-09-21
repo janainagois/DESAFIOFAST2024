758d572aacbcd46a0bd255dbaf0bfa03e84d989b
janainagois <marjangoisan@gmail.com>
2024-09-21 12:18:13 -0300

    adicionei mais variáveis ao arquivo variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,40 @@ variable "my_first_project" {
     type        = string
     default     = 552704724947
 }
+
+variable "google_instance_type" {
+  description = "Google Instance Type"
+  type        = string
+  default     = "e2-standard-2"
+  # default     = "e2-small"
+}
+
+variable "google_instance_image" {
+  description = "Google Instance Image"
+  type        = string
+  default     = "centos-cloud/centos-stream-9"
+}
+
+variable "google_network" {
+  description = "Google Network Name"
+  type        = string
+  default     = "default"
+}
+
+variable "ssh_key_path_default_user" {
+  description = "Path SSH Key for Default User"
+  type        = string
+  default     = "~/.ssh/ecdsa_ansible.pub"
+}
+
+variable "bucket_tfsatefile_name" {
+  description = "Name of the Bucket to save Statefile"
+  type        = string
+  default     = "bucket-statefile-fast2024"
+}
+
+variable "storage_class" {
+  description = "Name of the Bucket to save Statefile"
+  type        = string
+  default     = "standard"
+}
