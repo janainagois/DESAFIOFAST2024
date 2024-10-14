09ebdcc9cec3e35445d3562bd618469033915d2f
janainagois <marjangoisan@gmail.com>
2024-10-14 16:35:46 -0300

    testando cd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,7 +49,7 @@ variable "ssh_path_private_key" {
 
 variable "GOOGLE_CREDENTIALS" {
   type = string
-  default = "terraform/credential.json"
+  default = "./credential.json"
 }
 
 variable "bucket_tfsatefile_name" {
