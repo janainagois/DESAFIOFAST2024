3bb88bf67a93b871442cc780883dcf2b5461e9b9
janainagois <marjangoisan@gmail.com>
2024-10-14 16:39:15 -0300

    testando cd

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("./credential.json")
+  credentials = file("credential.json")
   project     = var.my_first_project
   region      = var.google_region
   zone        = var.google_zone
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,7 +49,7 @@ variable "ssh_path_private_key" {
 
 variable "GOOGLE_CREDENTIALS" {
   type = string
-  default = "./credential.json"
+  default = "credential.json"
 }
 
 variable "bucket_tfsatefile_name" {
